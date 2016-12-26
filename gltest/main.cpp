72197823ae5270585ccee0171b774ce50e00ccd2
mugkulee <mugku.lee@gmail.com>
2016-12-26 22:27:45 +0900

    gluLookAt TEST

--- gltest/main.cpp
+++ gltest/main.cpp
@@ -18,60 +18,59 @@
 
 using namespace std;
 
-static int Day = 0, Time = 0;
-
-void MyDisplay() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glColor3f(1.0, 0.3, 0.3);
-    glutWireSphere(0.2, 20, 16);
-    glPushMatrix();
-     glRotatef((GLfloat) Day, 0.0, 1.0, 0.0);
-     glTranslatef(0.7, 0.0, 0.0);
-     glRotatef((GLfloat) Time, 0.0, 1.0, 0.0);
-     glColor3f(0.5, 0.6, 0.7);
-     glutWireSphere(0.1, 10, 8);
-      glPushMatrix();
-       glRotatef((GLfloat) Time, 0.0, 1.0, 0.0);
-       glTranslatef(0.2, 0.0, 0.0);
-       glColor3f(0.9, 0.8, 0.2);
-       glutWireSphere(0.04, 10, 8);
-    glPopMatrix( );
-    glPopMatrix();
-    glutSwapBuffers();
+void InitLight() {
+    GLfloat mat_diffuse[]    = { 0.5, 0.4, 0.3, 1.0 };
+    GLfloat mat_specular[]   = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_ambient[]    = { 0.5, 0.4, 0.3, 1.0 };
+    GLfloat mat_shininess[]  = { 15.0 };
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_diffuse[]  = { 0.8, 0.8, 0.8, 1.0 };
+    GLfloat light_ambient[]  = { 0.3, 0.3, 0.3, 1.0 };
+    GLfloat light_position[] = { -3, 6, 3.0, 0.0 };
+    
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 }
 
-void MyKeyboard (unsigned char key, int x, int y) {
-    switch (key) {
-        case 'd':
-            Day = (Day + 10) % 360;
-            glutPostRedisplay();
-            break;
-        case 't':
-            Time = (Time + 5) % 360;
-            glutPostRedisplay();
-            break;
-        default:
-            break;
-    }
+void MyDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, -1.0, 2.0, 2.0, -1.0);
+    glutSolidTeapot(0.5);
+    glFlush();
+}
+
+void MyReshape(int w, int h) {
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(400, 400);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("OpenGL Sample Drawing");
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-//    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.0, 1.0);
+    glClearColor(0.4, 0.4, 0.4, 0.0);
+    InitLight();
     glutDisplayFunc(MyDisplay);
-    glutKeyboardFunc(MyKeyboard);
+    glutReshapeFunc(MyReshape);
     glutMainLoop();
     return 0;
+
 }
 
 
