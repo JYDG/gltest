457f59be79564d2a661b84461a9c65bbc58e7f89
mugkulee <mugku.lee@gmail.com>
2016-12-29 10:30:37 +0900

    Distortion prevention

--- gltest/main.cpp
+++ gltest/main.cpp
@@ -18,86 +18,34 @@
 
 using namespace std;
 
-int Width, Height;
-
-void Init() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity( );
-    
-    glOrtho(-2.0, 2.0, -2.0, 2.0, 0.5, 5.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity( );
-}
-
-void DrawScene() {
-    glColor3f(0.7, 0.7, 0.7);
-    glPushMatrix();
-    glTranslatef(0.0, -1.0, 0.0);
-    glBegin(GL_QUADS);
-    glVertex3f(2.0, 0.0, 2.0);
-    glVertex3f(2.0, 0.0, -2.0);
-    glVertex3f(-2.0, 0.0, -2.0);
-    glVertex3f(-2.0, 0.0, 2.0);
-    glEnd();
-    glPopMatrix();
-    
-    glColor3f(0.3, 0.3, 0.7);
-    glPushMatrix();
-    glTranslatef(0.0, 0.0, -0.5);
-    glutWireTeapot(1.0);
-    glPopMatrix();
-}
-
 void MyDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
-    glViewport(0, 0, Width/2, Height/2);
-    glPushMatrix();
-    gluLookAt(0.0, 0.0, 1.0,   0.0, 0.0, 0.0,   0.0, 1.0, 0.0);
-    DrawScene();
-    glPopMatrix();
-    glViewport(Width/2, 0, Width/2, Height/2);
-    glPushMatrix();
-    gluLookAt(1.0, 0.0, 0.0, 0.0, 0.0, 0.0,  0.0, 1.0, 0.0);
-    DrawScene();
-    glPopMatrix();
-    glViewport(0, Height/2, Width/2, Height/2);
-    glPushMatrix();
-    gluLookAt(0.0, 1.0, 0.0,   0.0, 0.0, 0.0,   0.0, 0.0, -1.0);
-    DrawScene();
-    glPopMatrix();
-    glViewport(Width/2, Height/2, Width/2, Height/2);
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    gluPerspective(30, 1.0, 3.0, 50.0);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    gluLookAt(5.0, 5.0, 5.0,  0.0, 0.0, 0.0,  0.0, 1.0, 0.0);
-    DrawScene();
-    glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
+    glColor3f(0.3, 0.3, 0.7);
+    glutWireTeapot(1.0);
     glFlush();
 }
 
 void MyReshape(int w, int h) {
-    Width = w;
-    Height = h;
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(30, (GLdouble) w / (GLdouble) h, 1.0, 50.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
-    Width = 500;
-    Height = 500;
-    glutInitDisplayMode(GLUT_SINGLE |GLUT_RGBA);
-    glutInitWindowSize(Width, Height);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowSize(500, 500);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("OpenGL Sample Drawing");
-    Init();
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     glutDisplayFunc(MyDisplay);
     glutReshapeFunc(MyReshape);
     glutMainLoop();
     return 0;
 }
+
 
