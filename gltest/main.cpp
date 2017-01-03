e24d0d30af1dd6f03d2afa10c47383b19556720c
mugkulee <mugku.lee@gmail.com>
2017-01-03 09:30:15 +0900

    draw

--- gltest/main.cpp
+++ gltest/main.cpp
@@ -18,10 +18,42 @@
 
 using namespace std;
 
+void InitLight() {
+    GLfloat mat_diffuse[]    = { 0.5, 0.4, 0.3, 1.0 };
+    GLfloat mat_specular[]   = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_ambient[]    = { 0.5, 0.4, 0.3, 1.0 };
+    GLfloat mat_shininess[]  = { 50.0 };
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_diffuse[]  = { 0.8, 0.8, 0.8, 1.0 };
+    GLfloat light_ambient[]  = { 0.3, 0.3, 0.3, 1.0 };
+    GLfloat light_position[] = { -3, 2, 3.0, 0.0 };
+    
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+}
+
+void InitVisibility() {
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
+    glEnable(GL_DEPTH_TEST);
+}
+
 void MyDisplay() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.3, 0.3, 0.7);
-    glutWireTeapot(1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.4, 0.5, 0.0, -0.5, -1.0, 0.0, 1.0, 0.0);
+    glutSolidTeapot(0.58);
     glFlush();
 }
 
@@ -29,19 +61,18 @@ void MyReshape(int w, int h) {
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(30, (GLdouble) w / (GLdouble) h, 1.0, 50.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-    glutPostRedisplay();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 2.0);
 }
 
 int main(int argc, char** argv) {
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutInitWindowSize(500, 500);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(700, 700);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("OpenGL Sample Drawing");
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClearColor(0.5, 0.5, 0.5, 0.0);
+    InitLight();
+    InitVisibility();
     glutDisplayFunc(MyDisplay);
     glutReshapeFunc(MyReshape);
     glutMainLoop();
