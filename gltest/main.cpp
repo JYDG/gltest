90c4784a4965e25ea3d90f32f4515ad5f48d50b9
mugkulee <mugku.lee@gmail.com>
2017-01-12 10:55:37 +0900

    와이어 프레임

--- gltest/main.cpp
+++ gltest/main.cpp
@@ -13,129 +13,44 @@
 #include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
-#include <OpenGL/glaux.h>
 
 //#include "hw5.h"
 
 using namespace std;
 
-GLfloat xrot = 0.9f, yrot = 0.8f, zrot = 1.0f;
-unsigned int MyTextureObject[1];
-AUX_RGBImageRec* pTextureImage[1];  //텍스쳐 저장 공간을 가리키는 포인터
+GLUquadricObj* qobj;
 
-void MyReshape(int w, int h) {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(40.0, (GLfloat) w / (GLfloat) h, 1.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-}
-
-void MyDisplay() {
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    glBindTexture(GL_TEXTURE_2D, MyTextureObject[0]);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);  //앞면
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  //뒷면
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);  //윗면
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  //아랫면
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);  //우측면
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  //좌측면
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-    glEnd();
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f (0., 1., 1.);
+    gluSphere(qobj, 1., 20, 20);        //반지름 1, 슬라이스 20개, 스택 20개
     glutSwapBuffers();
 }
 
-void MyTimer(int Value) {
-    glRotatef(xrot,1.0f,0.0f,0.0f);
-    glRotatef(yrot,0.0f,1.0f,0.0f);
-    glRotatef(zrot,0.0f,0.0f,1.0f);
-    glutPostRedisplay();
-    glutTimerFunc(50, MyTimer, 1);
+void gfxinit() {
+    qobj = gluNewQuadric();                 //새로운 곡면 객체 생성
+    gluQuadricDrawStyle(qobj, GLU_LINE);    //와이어 프레임 렌더링
 }
 
-AUX_RGBImageRec* LoadBMP(char* szFilename) {
-    FILE* pFile = NULL;
-    if (!szFilename) {
-        return NULL;
-    }
-    pFile = fopen(szFilename, "r");
-    if (pFile) {
-        fclose(pFile);
-        return auxDIBImageLoad(szFilename);     //파일로부터 메모리로
-    }
-    return NULL;
-}
-
-int LoadGLTextures(char* szFilePath){       //파일을 로드하고 텍스쳐로 변환
-    int Status = FALSE;
-    glClearColor(0.0, 0.0, 0.0, 0.5);
-    memset(pTextureImage, 0, sizeof(void*) * 1);    //포인터를 널로
-    
-    if (pTextureImage[0] = LoadBMP(szFilePath)) {   //비트맵을 로드하고 오류확인
-        Status = TRUE;                              //상태 플랙을 True로
-        glGenTextures(1, &MyTextureObject[0]);      //텍스쳐 생성
-        glBindTexture(GL_TEXTURE_2D, MyTextureObject[0]);
-        glTexImage2D(GL_TEXTURE_2D, 0, 3,
-                     pTextureImage[0]->sizeX, pTextureImage[0]->sizeY,
-                     0, GL_RGB, GL_UNSIGNED_BYTE, pTextureImage[0]->data);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-        glEnable(GL_TEXTURE_2D);
-    }
-    if (pTextureImage[0]) {                 //텍스쳐가 존재하면
-        if (pTextureImage[0]->data) {       //텍스쳐 영상이 존재하면
-            free(pTextureImage[0]->data);   //텍스쳐 영상공간 반납
-        }
-        free(pTextureImage[0]);             //텍스쳐 반납
-    }
-    return Status;
+void myReshape(int w, int h) {
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(40., (GLfloat) w / (GLfloat) h, 1., 10.);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0., 0., 5., 0., 0., 0., 0., 1., 0.);
+    glTranslatef(0., 0., -1.);
 }
 
 int main(int argc, char** argv) {
-    if (argc <= 1) {
-        printf("\n%s\n\n","Usage : TextureDLG3_Consol.exe [BMPFileName.bmp]");
-        exit(1);
-    }
-    else if (argc > 2) {
-        printf("\nToo Many Arguments!\nArgument is Only one\n");
-        exit(1);
-    }
-    else {
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-        glutCreateWindow("OpenGL Sample Program");
-        glutDisplayFunc(MyDisplay);
-        glutReshapeFunc(MyReshape);
-        glutTimerFunc(500, MyTimer, 1);
-        if (LoadGLTextures(argv[1])) {
-            glEnable(GL_TEXTURE_2D);
-            glShadeModel(GL_SMOOTH);
-            glClearDepth(1.0);
-            glEnable(GL_DEPTH_TEST);
-            glDepthFunc(GL_LEQUAL);
-            glHint(GL_PERSPECTIVE_CORRECTION_HINT,GL_NICEST);
-            glutMainLoop();
-        }
-    }
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutCreateWindow("simple");
+    glutReshapeFunc(myReshape);
+    glutDisplayFunc(display);
+    gfxinit();
+    glutMainLoop();
     
     return 0;
 }
